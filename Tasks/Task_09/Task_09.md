81ecafec15bdc2b5b6436f337b236c1bb9e7f7dc
Bastian Hodapp <bastianhodapp@gmail.com>
2022-05-23 14:47:24 +0200

    add task 9 stuff

---
+++ Tasks/Task_09/Task_09.md
@@ -0,0 +1,8 @@
+# Task 09
+
+- Using the npm mongodb package for using MongoDB
+- Environment variable handling with node.js seems a little more difficult...
+  - Actually, env variables from kubernetes can be passed directly into node's .env!
+    - Which makes it rather easy to access
+- Using [https://www.postman.com/](Postman) for test requests
+  - Support for local and dev environments with different urls and such
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,43 @@ import express = require("express");
 import os from "os";
 import initHealthChecksWithServer from "./HealthChecks";
 const Prometheus = require("prom-client");
-const logger = require('pino')()
-
-const basePath = "/hodappba";
+const logger = require("pino")();
+import bodyParser from "body-parser";
 
 //dotenv
 require("dotenv").config();
 
+const basePath = "/hodappba";
+
+//MongoDB
+import { Collection, Db, MongoClient, ObjectId } from "mongodb";
+import User from "./User";
+// Connection URL
+const url: string = process.env.MONGODB_HOST || "";
+const client = new MongoClient(url);
+let db: Db;
+let userCollection: Collection;
+
+// Database Name
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    logger.info("Connected successfully to database server");
+    db = client.db("cnc");
+    userCollection = db.collection("users");
+}
+
+main()
+    .then((e) => logger.info(e))
+    .catch((e) => logger.error(e));
+
+//Express
 const app = express();
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 const port = process.env.PORT || 443; // default port to listen
 
 const apiMetrics = require("prometheus-api-metrics");
@@ -33,6 +62,39 @@ router.get("/host", (request, response) => {
     numberOfHostCalls.inc();
     logger.info("Called hostname route.");
     response.send(`The hostname is: ${os.hostname()}`);
+});
+
+router.get("/users", (request, response) => {
+    logger.info("Called users get route.");
+    userCollection.find({}).toArray((err, result) => {
+        response.send(JSON.stringify(result));
+    });
+});
+
+router.post("/users", (request, response) => {
+    logger.info("Called users post route.");
+    logger.info(request.body);
+    userCollection.insertOne({
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+    });
+    response.sendStatus(200);
+});
+
+router.delete("/users", (request, response) => {
+    logger.info("Called users delete route.");
+
+    const objectID = new ObjectId(request.body.id);
+
+    userCollection.find({ _id: objectID });
+    response.sendStatus(200);
+});
+
+router.delete("/deleteAllUsers", (request, response) => {
+    logger.info("Called delete all users delete route.");
+
+    userCollection.deleteMany({});
+    response.sendStatus(200);
 });
 
 app.use(basePath, router);
